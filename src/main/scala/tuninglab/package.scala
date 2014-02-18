5b01bfe553a20f453396a055dbf837cb6d5475a8
Crosson David <crosson.david@gmail.com>
2014-02-18 17:43:39 +0100

    new commit ;)

--- src/main/scala/tuninglab/package.scala
+++ src/main/scala/tuninglab/package.scala
@@ -2,4 +2,6 @@ package object tuninglab {
 
   def gc()=System.gc()
   def alloc(szMb:Int=25) = Array.fill[Byte](1024*1024*szMb)(0x1)
+  def primesGenerator() = new fr.janalyse.primes.PrimesGenerator[Long]()
+  
 }
