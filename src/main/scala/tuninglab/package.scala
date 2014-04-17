6ae6f9eba946798ed2fc48a3dc696a9735018001
Crosson David <crosson.david@gmail.com>
2014-04-17 16:37:35 +0200

    new feature : consume function

--- src/main/scala/tuninglab/package.scala
+++ src/main/scala/tuninglab/package.scala
@@ -3,35 +3,34 @@ import scala.concurrent._
 import scala.concurrent.duration._
 import fr.janalyse.unittools._
 import java.io._
-import java.net.{Socket}
+import java.net.{ Socket }
 import resource._
 import resource.ManagedResource
 
 package object tuninglab {
 
-  def sleep(howlong:fr.janalyse.unittools.DurationHelper) {Thread.sleep(howlong)}
+  def sleep(howlong: fr.janalyse.unittools.DurationHelper) { Thread.sleep(howlong) }
   def gc() = System.gc()
   def alloc(szMb: Int = 25) = Array.fill[Byte](1024 * 1024 * szMb)(0x1)
-  def alloc(howmuch:fr.janalyse.unittools.SizeHelper) = Array.fill[Byte](howmuch.toSize.toInt)(0x1)
-  
+  def alloc(howmuch: fr.janalyse.unittools.SizeHelper) = Array.fill[Byte](howmuch.toSize.toInt)(0x1)
+
   def primesGenerator() = new fr.janalyse.primes.PrimesGenerator[Long]()
 
   def now = System.currentTimeMillis
-  def howlong[T](proc: => T) = { val s = now; val r=proc; ((now - s).toDurationDesc, r) }
+  def howlong[T](proc: => T) = { val s = now; val r = proc; ((now - s).toDurationDesc, r) }
   def futurehowlong[T](proc: => Future[T]) = { howlong(Await.result(proc, 60.seconds)) }
 
-  def readall(reader:BufferedReader) = {
+  def readall(reader: BufferedReader) = {
     Stream.continually(reader.readLine()).takeWhile(_ != null).mkString("\n")
   }
-  def printall(reader:BufferedReader) = {
+  def printall(reader: BufferedReader) = {
     Stream.continually(reader.readLine()).takeWhile(_ != null).foreach(println)
     "END"
   }
-  
+
   // httpclient("localhost", 8080) (_.println("GET /check/123 HTTP/1.1\r\nHost:me.com\r\nConnection:Close\r\n\r\n"))
-  def httpclient(host:String="127.0.0.1",port:Int=80, inBuffSz:Int=8192)
-                (outproc:(PrintWriter)=>Any, inproc:(BufferedReader)=>String = readall) = {
-    var res=Option.empty[String]
+  def httpclient(host: String = "127.0.0.1", port: Int = 80, inBuffSz: Int = 8192)(outproc: (PrintWriter) => Any, inproc: (BufferedReader) => String = readall) = {
+    var res = Option.empty[String]
     def mkSocket = {
       val sock = new Socket(host, port)
       //sock.setReceiveBufferSize(inBuffSz)
@@ -42,61 +41,74 @@ package object tuninglab {
       outStream <- managed(cnx.getOutputStream)
       out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outStream)))
       inStream <- managed(new InputStreamReader(cnx.getInputStream))
-      in = new BufferedReader(inStream,inBuffSz)
+      in = new BufferedReader(inStream, inBuffSz)
     } {
-      
+
       outproc(out)
       out.flush()
       res = Some(inproc(in))
     }
     res
   }
-  
-  
-  def slowPost(host:String="127.0.0.1", port:Int=80) {
-    httpclient(host,port) { pout =>
-        pout.println(s"POST /primesui/config HTTP/1.1")
-        pout.println(s"Host: $host:$port")
-        pout.println(s"Accept: */*")
-        pout.println(s"Origin: http://$host:$port")
-        pout.println(s"Content-Type: application/x-www-form-urlencoded")
-        pout.println(s"Cache-Control: max-age=0")
-        pout.flush()
-        Thread.sleep(10*1000L) // => R
-        pout.println(s"Referer: http://$host:$port/primesui/config")
-        pout.println(s"Connection: close")
-        pout.println(s"Content-Length: 17")
-        pout.println("")
-        pout.flush()
-        Thread.sleep(10*1000L) // => W
-        pout.println("""usecache:selected""")
-        pout.flush()           // => W
-        Thread.sleep(10*1000L)
+
+  def slowPost(host: String = "127.0.0.1", port: Int = 80) {
+    httpclient(host, port) { pout =>
+      pout.println(s"POST /primesui/config HTTP/1.1")
+      pout.println(s"Host: $host:$port")
+      pout.println(s"Accept: */*")
+      pout.println(s"Origin: http://$host:$port")
+      pout.println(s"Content-Type: application/x-www-form-urlencoded")
+      pout.println(s"Cache-Control: max-age=0")
+      pout.flush()
+      Thread.sleep(10 * 1000L) // => R
+      pout.println(s"Referer: http://$host:$port/primesui/config")
+      pout.println(s"Connection: close")
+      pout.println(s"Content-Length: 17")
+      pout.println("")
+      pout.flush()
+      Thread.sleep(10 * 1000L) // => W
+      pout.println("""usecache:selected""")
+      pout.flush() // => W
+      Thread.sleep(10 * 1000L)
     }
   }
-  
-  def slowClient(host:String="127.0.0.1", port:Int=80, sleep:Int=10, size:Int=3*1024) = {
-    val res = httpclient(host, port, inBuffSz=10) (
-       {pout =>
-          pout.println(s"GET /primesui/big/$size HTTP/1.1")
-          pout.println(s"Host: $host:$port")
-          pout.println(s"Accept: */*")
-          pout.println(s"Cache-Control: max-age=0")
-          pout.println(s"Connection: close")
-          pout.println()},
-       {reader => 
-          Thread.sleep(sleep*1000) ; readall(reader)} )
-          
-    res.map(_.size/1024)
+
+  def slowClient(host: String = "127.0.0.1", port: Int = 80, sleep: Int = 10, size: Int = 3 * 1024) = {
+    val res = httpclient(host, port, inBuffSz = 10)(
+      { pout =>
+        pout.println(s"GET /primesui/big/$size HTTP/1.1")
+        pout.println(s"Host: $host:$port")
+        pout.println(s"Accept: */*")
+        pout.println(s"Cache-Control: max-age=0")
+        pout.println(s"Connection: close")
+        pout.println()
+      },
+      { reader =>
+        Thread.sleep(sleep * 1000); readall(reader)
+      })
+
+    res.map(_.size / 1024)
   }
-  
-  
-  def background[T](proc : => T):Future[T] = {
+
+  def background[T](proc: => T): Future[T] = {
     val pr = Promise[T]
     new Thread {
-      override def run() {pr.success(proc) }
+      override def run() { pr.success(proc) }
       start()
     }
     pr.future
   }
+
+  def consume(zzz: String = "1m",
+              depth: Int = 64,
+              a: String = new String(Array.fill[Byte](1024)(0x0))): Int = {
+    if (depth > 0) {
+      consume(zzz, depth - 1) + 1
+    } else {
+      sleep(zzz)
+      0
+    }
+  }
+  
+  
 }
